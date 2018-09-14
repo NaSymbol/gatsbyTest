4f52236e4d5d094b5e1359d04e4819e9b880e530
andy <symbol_for_success@hotmail.com>
2018-09-14 21:58:25 +1200

    2nd

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+
+
+
+import React from "react"
+//importing header component from component folder
+import Header from "../components/header"
+import Footer from "../components/footer"
+
+
+
+
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header />
+    <p>Such wow. Very React.</p>
+    <Footer />
+  </div>
+  
+)
